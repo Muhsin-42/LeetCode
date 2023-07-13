7c70ea6f45b63cfda69fd50b5cc579e5ba89d5bc
Muhsin-42 <muhsinofficia21@gmail.com>
2023-07-13 19:44:17 +0530

    14-longest-common-prefix-EASY

---
+++ EASY/14-Longest-Common-Prefix/14-Longest-Common-Prefix.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let arr = strs.map((str)=>str.length)
+    let min = Math.min(...arr)
+    let flag = 0;
+    let res = [];
+    for(let j = 0; j<min; j++){
+        let curr = strs[0][j];
+        for(let i=0; i< strs.length; i++){
+            if(strs[i][j]!=curr)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag) break
+         res.push(strs[0][j])  
+    }
+    return res.join('')
+
+};
