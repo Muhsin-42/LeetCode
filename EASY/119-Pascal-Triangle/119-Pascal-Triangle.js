22a94d4dd32186bd0a9bdd482922a67f6861cc94
Muhsin-42 <muhsinofficial21@gmail.com>
2023-07-22 23:57:57 +0530

    update

--- EASY/119-Pascal-Triangle/119-Pascal-Triangle.js
+++ EASY/119-Pascal-Triangle/119-Pascal-Triangle.js
@@ -4,9 +4,10 @@
  */
 var generate = function(numRows) {
     let pasc=[];
+    //the code
     for(let i=0; i<numRows;i++){
         for(let j=0; j<=i ; j++){
-            if(j===0) pasc.push([1]);
+            if(j===0) pasc.push([1]); 
             else if(j===i) pasc[i].push(1)
             else pasc[i].push(pasc[i-1][j-1]+pasc[i-1][j])
         }
